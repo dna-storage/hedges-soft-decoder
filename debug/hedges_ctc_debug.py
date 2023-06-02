cb3ee2d430a41b73f8d9cfba43a1387c83b70107
kvolkel <kvolkel@ncsu.edu>
2023-06-02 12:07:45 -0400

    add debug

---
+++ debug/hedges_ctc_debug.py
@@ -0,0 +1,18 @@
+import bonito.hedges_decode as hd
+import bonito.hedges_decode_numpy as hdnp
+
+if __name__ =="__main__":
+    import pickle
+    import sys
+    import json
+    assert(len(sys.argv)==3)
+    debug_data = pickle.load(open(sys.argv[1],"rb"))
+    alphabet = ["N","A","C","G","T"]
+    b = bytes([204,0])
+    endpoint_str="GGCGACAGAAGAGTCAAGGTTC"
+    #quick debug of decoding
+    for read,scores in debug_data:
+        print("Decoding Read {}".format(read))
+        print(scores.size())
+        x=hd.hedges_decode(read,{"scores":scores},sys.argv[2],b,False,alphabet,1,endpoint_str)
+        print(x['sequence'])
