4ab665ba00b3b938b6164d094beb0ae017ffc016
kvolkel <kvolkel@ncsu.edu>
2023-06-14 18:26:50 -0400

    make contexts for contextmanager

--- bonito/hedges_decode/cuda/index_gather.cu
+++ bonito/hedges_decode/cuda/index_gather.cu
@@ -15,8 +15,8 @@ extern "C" __global__ void gather_scores(
 {
     int dim0_idx = threadIdx.y+blockDim.y*blockIdx.x, dim1_idx = threadIdx.x;
     if(dim0_idx>=dim0_size) return; //don't do anything
-    int target_dim0 = dim0[dim0_idx*dim1_size+dim1_idx]
-    int target_dim1 = dim1[dim0_idx*dim1_size+dim1_idx]
-    out[dim0_idx*dim1_size+dim1_idx] = target[target_dim0*target_dim1_size+target_dim1]
+    int target_dim0 = dim0[dim0_idx*dim1_size+dim1_idx];
+    int target_dim1 = dim1[dim0_idx*dim1_size+dim1_idx];
+    out[dim0_idx*dim1_size+dim1_idx] = target[target_dim0*target_dim1_size+target_dim1];
 }
 
