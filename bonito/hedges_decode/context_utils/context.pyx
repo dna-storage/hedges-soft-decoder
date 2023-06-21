ac479f34b9278928e2cb338f6773b06e40621675
kvolkel <kvolkel@ncsu.edu>
2023-06-21 17:09:28 -0400

    debugging mod code

--- bonito/hedges_decode/context_utils/context.pyx
+++ bonito/hedges_decode/context_utils/context.pyx
@@ -12,6 +12,7 @@ DTYPE=np.int64
 ctypedef cnp.int64_t DTYPE_t
 
 
+
 cdef class ContextManager:
    cdef void** _contexts
    cdef int _H
@@ -83,7 +84,7 @@ def fill_base_transitions(int H, int n_edges, ContextManager c, int nbits, bool 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def mod_mask_states(ContextManager c,int nbits, int num_mods):
-    cdef cnp.ndarray[DTYPE_t,ndim=2] mod_mask= np.zeros([c._H, (1<<nbits)*num_mods], dtype=DTYPE )
+    cdef cnp.ndarray[cnp.int8_t,ndim=2] mod_mask= np.zeros([c._H, (1<<nbits)*num_mods], dtype=bool )
     cdef int i
     cdef int j
     cdef void* context
